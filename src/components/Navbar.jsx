f1247c6ba4cdb38c48bf9f24277861afb0540093
Dragon03K <mohammadarbaz320@gmail.com>
2025-11-24 13:57:09 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "#constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="Home Logo" />
+        <p className="font-bold">Arbaz's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name, link }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} className="icon-hover" />
+            </li>
+          ))}
+        </ul>
+        <time datetime="">{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
