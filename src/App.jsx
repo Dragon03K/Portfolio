630b72dd4d15c7d8b6a9bfb845fe0ecbaa5240c4
Dragon03K <mohammadarbaz320@gmail.com>
2025-11-27 15:57:19 +0530

    feat: introduce Home component with draggable project folders and integrate various window components into App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Draggable } from "gsap/Draggable";
 import gsap from "gsap";
 
-import { Navbar, Welcome, Dock } from "#components";
+import { Navbar, Welcome, Dock, Home } from "#components";
 import {
   Finder,
   Resume,
@@ -18,7 +18,7 @@ const App = () => {
     <main>
       <Navbar />
       <Dock />
-
+      <Welcome />
       <Terminal />
       <Safari />
       <Resume />
@@ -26,6 +26,7 @@ const App = () => {
       <Text />
       <Image />
       <Contact />
+      <Home />
     </main>
   );
 };
