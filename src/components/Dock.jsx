50090742e49fa981d823c4918684a3447991f227
Dragon03K <mohammadarbaz320@gmail.com>
2025-11-27 12:52:14 +0530

    open and close the bottom navigation

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,10 +1,12 @@
 import { dockApps } from "#constants";
+import useWindowStore from "#store/window";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useRef } from "react";
 import { Tooltip } from "react-tooltip";
 
 const Dock = () => {
+  const { openWindow, closeWindow, windows } = useWindowStore();
   const dockRef = useRef(null);
 
   useGSAP(() => {
@@ -55,7 +57,21 @@ const Dock = () => {
     };
   }, []);
 
-  const toggleApp = () => {};
+  const toggleApp = (app) => {
+    if (!app.canOpen) return;
+    const window = windows[app.id];
+
+    if (!window) {
+      console.error(`Window ${app.id} not found`);
+      return;
+    }
+
+    if (window.isOpen) {
+      closeWindow(app.id);
+    } else {
+      openWindow(app.id);
+    }
+  };
   return (
     <section id="dock">
       <div ref={dockRef} className="dock-container">
